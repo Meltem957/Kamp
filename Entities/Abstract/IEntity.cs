9d2619c662c6bc74e52f0f9e1608544346c8aa14
Meltem957 <78206014+Meltem957@users.noreply.github.com>
2021-01-31 00:06:44 +0300

    ...

---
+++ Entities/Abstract/IEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Abstrack
+{
+    public class IEntity
+    {
+        //IEntity implement eden class veritabanıdır.
+        public interface Entity
+    }
+}
